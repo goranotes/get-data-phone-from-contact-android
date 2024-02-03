deb98e76b5b929ae1d43bb7a7cb1f3d10a41feb5
gora <anggara.dwikuntoro@gmail.com>
2024-02-03 10:32:35 +0700

    [Added] add contact icon and clicked

--- app/src/main/java/com/example/getdataphonefromcontact/MainActivity.kt
+++ app/src/main/java/com/example/getdataphonefromcontact/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.getdataphonefromcontact
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.getdataphonefromcontact.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +13,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.ivContactPhone.setOnClickListener {
+            Toast.makeText(this, "masuk",Toast.LENGTH_SHORT).show()
+        }
     }
 }
